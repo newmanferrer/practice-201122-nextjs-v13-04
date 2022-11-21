a3b8dd718039425da5c334681ff9b7a2c51e9cf5
Newman Ferrer <newmanferrer@gmail.com>
2022-11-20 20:34:46 -0400

    feat: :sparkles: add images configurations
    add images configurations

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   experimental: {
     appDir: true
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/api/products'
+      }
+    ]
   }
 }
 
