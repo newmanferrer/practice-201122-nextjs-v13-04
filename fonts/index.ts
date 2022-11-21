b9af0d46b55b4967e5a09b134c832865f8475ab4
Newman Ferrer <newmanferrer@gmail.com>
2022-11-20 20:46:34 -0400

    refactor: :recycle: add properties for the fonts
    add properties for the fonts

--- fonts/index.ts
+++ fonts/index.ts
@@ -16,18 +16,18 @@ export const font_rubik_distressed = Rubik_Distressed({
   variable: '--font-rubik-distressed'
 })
 
-//* Paragraph No.1
-export const font_dancing_script = Dancing_Script({
-  display: 'swap',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-dancing-script'
-})
-
-//* Paragraph No.2
+//* Paragraph No.1 | Navigation
 export const font_roboto = Roboto({
   display: 'swap',
   subsets: ['latin'],
   weight: ['400', '700', '900'],
   variable: '--font-roboto'
+})
+
+//* Paragraph No.2
+export const font_dancing_script = Dancing_Script({
+  display: 'swap',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-dancing-script'
 })
