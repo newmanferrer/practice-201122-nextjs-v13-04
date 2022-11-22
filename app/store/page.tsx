ca92142f867603520e4c603299fbc04070c35dbd
Newman Ferrer <newmanferrer@gmail.com>
2022-11-22 17:12:36 -0400

    refactor: :recycle: page and css
    page and css

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,20 +1,16 @@
-import Image from 'next/image'
-import { getProductsLatest } from '../../services'
+import { getProductsIncrement } from '../../services'
+import { ProductCard } from '../../components'
 import styles from './page.module.css'
 
 export default async function Store() {
-  const products = await getProductsLatest()
+  const products = await getProductsIncrement()
 
   return (
     <div className={styles.container}>
       <h1>Store</h1>
-      {products &&
-        products.map(product => (
-          <div key={product.id}>
-            <Image width={400} height={400} src={product.picture} alt={product.name} />
-            {product.name}
-          </div>
-        ))}
+      <section>
+        {products && products.map(product => <ProductCard key={product.id} product={product} />)}
+      </section>
     </div>
   )
 }
