6a25237d27a36fb54c1b387bae8f4f8ad0169fb9
Newman Ferrer <newmanferrer@gmail.com>
2022-11-22 17:17:37 -0400

    feat: :sparkles: create ProductDetailsCard and styles css
    create ProductDetailsCard and styles css

---
+++ components/ProductDetailsCard/ProductDetailsCard.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { IProduct } from '../../models'
+import styles from './ProductDetailsCard.module.css'
+
+interface IProductDetailsCardProps {
+  product: IProduct
+}
+
+export const ProductDetailsCard = ({ product }: IProductDetailsCardProps) => {
+  return (
+    <article className={styles.article}>
+      <Image src={product.picture} alt={product.name} width={300} height={300} />
+      <h2>{product.name}</h2>
+      <h3>${product.price}</h3>
+      <h3>quantity: {product.stock}</h3>
+      <h3>brand: {product.brand_name}</h3>
+      <p>{product.description}</p>
+      <Link href='/store'>Go to products</Link>
+      <button>Add to Cart</button>
+    </article>
+  )
+}
