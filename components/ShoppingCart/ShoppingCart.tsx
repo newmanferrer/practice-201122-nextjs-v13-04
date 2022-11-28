f88cd8f7037cd546772b6829196e9f75f9099858
Newman Ferrer <newmanferrer@gmail.com>
2022-11-28 17:08:49 -0400

    refactor: :chart_with_upwards_trend: refactor ShoppingCart Component and Styles
    refactor ShoppingCart Component and Styles

--- components/ShoppingCart/ShoppingCart.tsx
+++ components/ShoppingCart/ShoppingCart.tsx
@@ -1,12 +1,28 @@
+'use client'
+
+import { useShoppingCartContext } from '../../contexts'
+import { CartProductCard } from '../'
 import styles from './ShoppingCart.module.css'
 
 export const ShoppingCart = () => {
+  const { cart, isOpen, openCart, closeCart, toggleCart } = useShoppingCartContext()
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ display: isOpen ? 'block' : 'none' }}>
       <div className={styles.title_button_container}>
         <h2>Shopping Cart</h2>
-        <button>❌</button>
+        <button onClick={toggleCart}>❌</button>
       </div>
+
+      {cart.length === 0 ? (
+        <div>
+          <h3>Cart is empty</h3>
+        </div>
+      ) : (
+        <h3>Your Products</h3>
+      )}
+
+      <div>{JSON.stringify(isOpen, null, 2)}</div>
     </div>
   )
 }
