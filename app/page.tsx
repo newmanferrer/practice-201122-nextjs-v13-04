a6739909285c146152176f153ca05fcafb98a725
Newman Ferrer <newmanferrer@gmail.com>
2022-11-29 22:36:40 -0400

    refactor: :recycle: refactor home page
    refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+import { getProductsLatest } from '../services'
+import { ProductCard } from '../components'
 import styles from './page.module.css'
 
-export default function Home() {
+export default async function Home() {
+  const productsLatest = await getProductsLatest()
+
   return (
     <div className={styles.container}>
-      <h1>Home Page</h1>
-      <p>This is a home page. My name is Newman Ferrer. This is a test.</p>
+      <div className={styles.banner}>
+        <div className={styles.bannerBackground}>
+          <div className={styles.bannerInfo}>
+            <h1>Best Sellers 2022 Collections</h1>
+            <p>Universe 2022. New hats, tees, sweatshirts and more.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.products}>
+        <h2>Latest Products</h2>
+        <section>
+          {productsLatest &&
+            productsLatest.map(product => <ProductCard key={product.id} product={product} />)}
+        </section>
+      </div>
     </div>
   )
 }
