a73250854f2c3634775b7a649f586b4e5caf6890
Newman Ferrer <newmanferrer@gmail.com>
2022-12-01 16:23:24 -0400

    refactor: :recycle: refactor component CartProductCard and styles css
    refactor component CartProductCard and styles css

--- components/CartProductCard/CartProductCard.tsx
+++ components/CartProductCard/CartProductCard.tsx
@@ -8,11 +8,23 @@ interface ICartProductCardProps {
 
 export const CartProductCard = ({ product }: ICartProductCardProps) => {
   return (
-    <article className={styles.article}>
-      <Image src={product.picture} alt={product.name} width={50} height={50} />
-      <h2>{product.name}</h2>
-      <h3>${product.price}</h3>
-      <h3>quantity: {product.quantity}</h3>
+    <article className={styles.article} key={product.id}>
+      <div className={styles.articleImage}>
+        <Image src={product.picture} alt={product.name} width={70} height={70} />
+      </div>
+
+      <div className={styles.articleInfo}>
+        <h4>{product.name}</h4>
+        <p>${product.price}</p>
+        <p>{product.quantity} units</p>
+        <p>SubTotal: ${(product.price * product.quantity).toFixed(2)}</p>
+      </div>
+
+      <div className={styles.articleButtons}>
+        <button>-1</button>
+        <button>Delete</button>
+        <button>+1</button>
+      </div>
     </article>
   )
 }
